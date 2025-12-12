fad524b634e84df00982e8a289d0d89a1e74a5db
ABDESSLAM ROUAIJEL <166573067+abdesslamrouaijel@users.noreply.github.com>
2025-12-13 00:30:08 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+// main.js
+var typed = new Typed(".text", {
+    strings: [
+        "future AI engineer",
+        "Data Science enthusiast",
+        "AI practitioner",
+        "problem solver",
+        "robotics enthusiast"
+    ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 600,
+    loop: true
+});
